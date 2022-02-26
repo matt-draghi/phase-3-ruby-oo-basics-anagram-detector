d86a65ce7427dd84481377c1542bc5dad0f2cfd2
Matt Draghi <mtd5264@gmail.com>
2022-02-26 13:17:14 -0500

    commit tree ae975948f28b144207b5bbdb1785097278163726

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word, :matches
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_list)
+        @matches = word_list.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
